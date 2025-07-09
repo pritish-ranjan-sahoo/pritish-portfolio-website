ae25ef92e18bc7e45728a23354fdead5f97e9995
Pritish <codecharlie404@gmail.com>
2025-07-09 23:31:25 +0530

    Updated My Resume

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -32,7 +32,7 @@ export const Home = () => {
             </a>
 
             <a
-              href="/Resume_Pritish.pdf"
+              href="/CV_Pritish_Ranjan_Sahoo.pdf"
               target="_blank"
               className="flex justify-center items-center gap-2 border md:text-lg border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
