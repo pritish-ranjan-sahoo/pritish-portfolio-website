2db492f6c878fcaa7a441d050c3c079f9c20f37e
Pritish <codecharlie404@gmail.com>
2025-07-15 01:17:52 +0530

    Fixed UI errors

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -5,7 +5,7 @@ export default function EducationCard({ education }) {
     <ul className="list-disc list-outside text-gray-300 mx-3">
       {education.map((edu, index) => (
         <li className="space-y-0.5 my-5" key={index}>
-          <p className="flex justify-between items-center">
+          <p className="flex flex-col justify-between items-start">
             <strong className="text-lg font-semibold capitalize">
               {edu.type}
             </strong>
